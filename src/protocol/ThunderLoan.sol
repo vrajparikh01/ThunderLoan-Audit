dc7bd753edd8ee249a7be018ba0bb47f1348483a
Vraj Parikh <vrajaparikh01@gmail.com>
2026-07-29 16:53:27 +0530

    feat: audit findings with layout
    Signed-off-by: Vraj Parikh <vrajaparikh01@gmail.com>

--- src/protocol/ThunderLoan.sol
+++ src/protocol/ThunderLoan.sol
@@ -235,6 +235,7 @@ contract ThunderLoan is Initializable, OwnableUpgradeable, UUPSUpgradeable, Orac
     }
 
     // @audit-info zero address check 
+    // @audit-low you can't use repay to repay the loan inside another flash loan
     function repay(IERC20 token, uint256 amount) public {
         if (!s_currentlyFlashLoaning[token]) {
             revert ThunderLoan__NotCurrentlyFlashLoaning();
